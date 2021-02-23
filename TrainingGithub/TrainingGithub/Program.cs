98872a87baad57bcc0239238292c86746adf4587
Mihail Maksimov <davilkafm@yandex.ru>
2021-02-23 12:37:30 +0300

    Hello :)
    Ahhahahha

--- TrainingGithub/TrainingGithub/Program.cs
+++ TrainingGithub/TrainingGithub/Program.cs
@@ -6,7 +6,7 @@ namespace TrainingGithub
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello :)");
         }
     }
 }
