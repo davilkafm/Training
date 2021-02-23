c98a7b8c3f70beafa9d10926bb92afdeb999fc33
Mihail Maksimov <davilkafm@yandex.ru>
2021-02-23 12:44:44 +0300

    Create IModel

---
+++ TrainingGithub/TrainingGithub/IModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TrainingGithub
+{
+    /// <summary>
+    /// Протокол для разработки подсчета периметра прямоугольника
+    /// </summary>
+    public interface IModel
+    {
+        /// <summary>
+        /// Метод, возвращающий периметр прямоугольникапо двум сторонам
+        /// </summary>
+        /// <returns>Возвращает периметр прямоугольника</returns>
+        /// <param name="a">Ширина прямоугольника</param>
+        /// <param name="b">Высота прямоугольника</param>
+        int GetP(int a, int b);
+    }
+}
