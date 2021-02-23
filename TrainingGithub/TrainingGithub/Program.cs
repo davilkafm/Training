a9013a77e506bbbd31ad2ff91300e807a444165e
Mihail Maksimov <davilkafm@yandex.ru>
2021-02-23 12:34:24 +0300

    Begin
    :)

---
+++ TrainingGithub/TrainingGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrainingGithub
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
